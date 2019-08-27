74c64232bb3a7db8705f444fea44d772c1fe083c
Kavya1709 <kavyaprakash17@gmail.com>
2019-08-27 21:53:23 +1000

    Full commit

---
+++ Competition/MarsFramework/MarsFramework/Pages/ShareSkill.cs
@@ -0,0 +1,157 @@
+﻿using AutoIt;
+using MarsFramework.Global;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MarsFramework.Pages
+{
+    internal class ShareSkill
+    {
+        public ShareSkill()
+        {
+            PageFactory.InitElements(Global.GlobalDefinitions.driver, this);
+        }
+
+        //Find ShareSkill
+        [FindsBy(How = How.XPath, Using = "//a[@class='ui basic green button']")]
+        private IWebElement Share_skill { set; get; }
+
+        //Find Title
+        [FindsBy(How = How.XPath, Using = "//input[@name='title']")]
+        private IWebElement Title { set; get; }
+
+        //Find Description 
+        [FindsBy(How = How.XPath, Using = "//textarea[@name='description']")]
+        private IWebElement Description { set; get; }
+
+        //Select Category
+        [FindsBy(How = How.XPath, Using = "//select[@name='categoryId']")]
+        private IWebElement Category { set; get; }
+
+        //Select Sub Category               
+        [FindsBy(How = How.XPath, Using = "//select[@name='subcategoryId']")]
+        private IWebElement SubCategory { set; get; }
+
+        //Find tag
+        [FindsBy(How = How.XPath, Using = "//div[@class='tooltip-target ui grid']/div[@class='twelve wide column']/div/div/div/div/input[@class='ReactTags__tagInputField']")]
+        private IWebElement Tags { set; get; }
+
+        //Find Service Type
+        [FindsBy(How = How.XPath, Using = "//input[@name='serviceType'][@value='1']")]
+        private IWebElement ServiceType { set; get; }
+
+        //Find Location Type
+        [FindsBy(How = How.XPath, Using = "//input[@name='locationType'][@value='1']")]
+        private IWebElement locationType { set; get; }
+
+        //Find Skill Trade
+        [FindsBy(How = How.XPath, Using = "//input[@name='skillTrades'][@value='true']")]
+        private IWebElement skillTrades { set; get; }
+
+        //Find Available days 
+        [FindsBy(How = How.XPath, Using = " ")]
+        private IWebElement Available_days { set; get; }
+
+        //Find Skill-Exchange
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"service-listing-section\"]/div[2]/div/form/div[8]/div[4]/div/div/div/div/div/input")]
+        private IWebElement SkillExchange { set; get; }
+
+        //Find Work Samples
+        [FindsBy(How = How.XPath, Using = "//i[@class='huge plus circle icon padding-25']")]
+        private IWebElement Work_Samples { set; get; }
+
+        //Find Active
+        [FindsBy(How = How.XPath, Using = "//input[@name='isActive'][@value='true']")]
+        private IWebElement Active { set; get; }
+
+        //Find Save
+        [FindsBy(How = How.XPath, Using = "//input[@value='Save']")]
+        private IWebElement Save { set; get; }
+
+
+
+        internal void Share_Skill()
+        {
+            //extent Reports
+            // Base.test = Base.extent.StartTest("Login Test");
+
+            //Click on ShareSkill
+            Share_skill.Click();
+            Thread.Sleep(5000);
+
+            //Populate the Excel Sheet
+            GlobalDefinitions.ExcelLib.PopulateInCollection(Base.ExcelPath, "ShareSkill");
+            Thread.Sleep(1000);
+
+            //Enter Title
+            Title.SendKeys(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Title"));
+            Thread.Sleep(1000);
+
+            //Enter Description 
+            Description.SendKeys(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Description"));
+            Thread.Sleep(1000);
+
+            //Click on Category
+            Category.Click();
+
+            //Enter Category            
+            var selectElement = new SelectElement(Category);
+            selectElement.SelectByText(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Category"));
+            Thread.Sleep(5000);
+
+            //Click on Sub Category
+            SubCategory.Click();
+            Thread.Sleep(3000);
+
+            //Enter Sub Category
+            var selectElementSub = new SelectElement(SubCategory);
+            selectElementSub.SelectByText(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Sub Category"));
+            Thread.Sleep(1000);
+
+            //Click on tags
+            Tags.Click();
+
+            //Enter a tag
+            Tags.SendKeys(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Tags"));
+
+            //Select a Service Type
+            ServiceType.SendKeys(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Service Type"));
+
+            //Select Location Type
+            locationType.SendKeys(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Location Type"));
+
+            //Select Skill Trade
+            skillTrades.SendKeys(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Skill Trade"));
+
+            //Select Skill-Exchange
+            SkillExchange.SendKeys(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Skill Exchange"));
+
+            Thread.Sleep(2000);
+
+            //Click on plus icon of Work Samples
+            Work_Samples.Click();
+
+            Thread.Sleep(3000);
+
+            //Call AutoIt to choose a file
+            AutoItX.ControlFocus("Open", "", "Edit1");
+            AutoItX.ControlSetText("Open", "", "Edit1", @"D:\kavya\AutoIt");
+            AutoItX.ControlClick("Open", "", "Button1");
+
+            //Select Active
+            Active.SendKeys(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Active"));
+
+            //Click on Save
+            Save.Click();
+
+        }
+
+    }
+}
---
+++ Competition/MarsFramework/MarsFramework/Test/Program.cs
@@ -0,0 +1,45 @@
+﻿using MarsFramework.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsFramework
+{
+    [TestFixture]
+    //[Category("Sprint 1")]
+
+    class Program : Global.Base
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("in main start");
+            Program p1 = new Program();
+            p1.UserAccount();
+        }
+
+
+        [Test]
+        public void UserAccount()
+        {
+
+            // Create an class and object to call the method
+            SignIn sign_In = new SignIn();
+            sign_In.LoginSteps();
+        }
+
+            //Create an class and object to call the method
+            // Profile obj = new Profile();
+            // obj.EditProfile();
+        [Test]
+        public void ShareSkill()
+        { 
+            //Create an Object to call the method
+            ShareSkill obj1 = new ShareSkill();
+            obj1.Share_Skill();
+        }
+    }
+}
